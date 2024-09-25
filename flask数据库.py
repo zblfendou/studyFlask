39cb1999e39927f2a4a9d9792ba6655c8ee24e9c
zhangbinlei <zhangbinlei@xinnet.com>
2024-09-25 11:55:07 +0800

    flask mysql success

--- flask数据库.py
+++ flask数据库.py
@@ -1,6 +1,7 @@
-from flask import Flask
+import json
+
+from flask import Flask, jsonify, Response
 from flask_sqlalchemy import SQLAlchemy
-from statsmodels.sandbox.distributions.genpareto import method
 
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ app.config['SECRET_KEY'] = '1234'  # 密码
 # 端口：3306
 # 数据库名：flask_db #这里的数据库需要提前建好
 app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://selfsiteuser:selfsiteuserpassword1QASW2@10.24.46.117:3306/flask_db'
+    'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://selfsiteuser:selfsiteuserpassword1QASW2@10.24.46.117:3306/flask_db?charset=utf8'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
 
@@ -24,8 +25,12 @@ class Role(db.Model):
     name = db.Column(db.String(64), unique=True)  # name字段，字符串类型，唯一
     users = db.relationship('User', backref='role', lazy='dynamic')  # 外键关系，动态更新
 
-    def __repr__(self):  # 相当于toString
-        return '<Role %r>' % self.name
+    def to_dict(self):
+        return {'id': self.id,
+                'name': self.name}
+
+    def __repr__(self):
+        return '<Role(id="%s", name="%s")>' % (self.id, self.name)
 
 
 # 新建表User
@@ -36,7 +41,7 @@ class User(db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))  # 外键
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return "<User(id='%s', username='%s')>" % (self.id, self.username)
 
 
 @app.route('/initDb', methods=['GET', 'POST'])
@@ -48,6 +53,22 @@ def initDb():
     db.create_all()
     print("Tables created successfully!")
     return 'ok'
+
+
+@app.route('/addRole', methods=['POST', 'GET'])
+def add_role():
+    role = Role(name='管理员1')
+    db.session.add(role)
+    db.session.commit()
+    return 'add success'
+
+
+@app.route('/queryRoles', methods=['GET', 'POST'])
+def queryRoles():
+    role_all = db.session.query(Role).all()
+    roles_list = [role.to_dict() for role in role_all]
+    response_json = json.dumps(roles_list, ensure_ascii=False)
+    return Response(response_json, content_type='application/json;charset=utf-8')
 
 
 if __name__ == '__main__':
