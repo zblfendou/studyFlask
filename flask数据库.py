56fca336952ef08d1e7589e72794092732664b81
zhangbinlei <zhangbinlei@xinnet.com>
2024-09-25 10:03:17 +0800

    flask mysql success

--- flask数据库.py
+++ flask数据库.py
@@ -1,54 +1,54 @@
-# flask 数据库
-import os
-import sys
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-WIN = sys.platform.startswith('win')
-if WIN:  # 如果是 Windows 系统，使用三个斜线
-    prefix = 'sqlite:///'
-else:  # 否则使用四个斜线
-    prefix = 'sqlite:////'
+from statsmodels.sandbox.distributions.genpareto import method
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
-# 在扩展类实例化前加载配置
+
+app.config['SECRET_KEY'] = '1234'  # 密码
+# 协议：mysql+pymysql
+# 用户名：selfsiteuser
+# 密码：selfsiteuserpassword1QASW2
+# IP地址：10.24.46.117
+# 端口：3306
+# 数据库名：flask_db #这里的数据库需要提前建好
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://selfsiteuser:selfsiteuserpassword1QASW2@10.24.46.117:3306/flask_db'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
 
 
-# 数据模型
+# 新建表Role
+class Role(db.Model):
+    __tablename__ = 'roles'  # 表名
+    id = db.Column(db.Integer, primary_key=True)  # id字段，int类型，主键
+    name = db.Column(db.String(64), unique=True)  # name字段，字符串类型，唯一
+    users = db.relationship('User', backref='role', lazy='dynamic')  # 外键关系，动态更新
+
+    def __repr__(self):  # 相当于toString
+        return '<Role %r>' % self.name
+
+
+# 新建表User
 class User(db.Model):
-    '''用户表'''
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(64), unique=True, index=True)  # 索引
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))  # 外键
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
-class Movie(db.Model):
-    '''电影表'''
-    __tablename__ = 'movie'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50), nullable=False)
-    # 表关系 ForeignKey 用来关联到另外一张表
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+@app.route('/initDb', methods=['GET', 'POST'])
+def initDb():
+    print("Starting the application...")
+    print("Before creating tables...")
+    db.drop_all()
+    print("Tables dropped successfully!")
+    db.create_all()
+    print("Tables created successfully!")
+    return 'ok'
 
 
 if __name__ == '__main__':
-    # 清除所有的表
-    db.drop_all()
-    # 创建所有的表
-    db.create_all()
-
-    # 创建对象插入数据
-    user1 = User(name='zhangsan', email='123@qq.com')
-    user2 = User(name='lisi', email='456@qq.com')
-    db.session.add_all([user1, user2])
-
-    movie1 = Movie(title='霸王别姬', user_id=user1.id)
-    movie2 = Movie(title='大话西游', user_id=user2.id)
-    db.session.add_all([movie1, movie2])
-    # 提交事务
-    db.session.commit()
+    app.run(debug=True)
--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-"""
+'''
     字符串拼接
-"""
+'''
 # radius_ = float(input("Enter radius: "))
 # perimeter = 2 * 3.1416 * radius_
 # area = 3.1416 * radius_ * radius_
 # print("周长: %.2f" % perimeter)
 # print("Area: %.2f" % area)
-# """
+# '''
 #     身份验证
-# """
+# '''
 # username = input("Enter username: ")
 # password = input("Enter password: ")
 # if username == "admin" and password == "123456":
@@ -16,12 +16,12 @@
 # else:
 #     print("Login failed!")
 
-"""
+'''
     分段函数求值
         3x - 5  (x > 1)
 f(x) =  x + 2   (-1 <= x <= 1)
         5x + 3  (x < -1)    
-"""
+'''
 # x = float(input("x = "))
 # if x > 1:
 #     y = 3 * x - 5
@@ -31,12 +31,12 @@ f(x) =  x + 2   (-1 <= x <= 1)
 #     y = x + 2
 # print("f(%.2f) = %.2f" % (x, y))
 
-"""
+'''
         分段函数求值进阶
         3x - 5  (x > 1)
 f(x) =  x + 2   (-1 <= x <= 1)
         5x + 3  (x < -1)    
-"""
+'''
 # x = float(input("x = "))
 # if x > 1:
 #     y = 3 * x - 5
@@ -47,9 +47,9 @@ f(x) =  x + 2   (-1 <= x <= 1)
 #         y = 5 * x + 3
 # print("f(%.2f) = %.2f" % (x, y))
 
-"""
+'''
     英制单位英寸与公制单位厘米互换
-"""
+'''
 # value = float(input("请输入长度: "))
 # unit = input("请输入单位: ")
 # if unit == 'in' or unit == '英寸':
@@ -59,10 +59,10 @@ f(x) =  x + 2   (-1 <= x <= 1)
 # else:
 #     print("请输入有效的单位")
 
-"""
+'''
     百分制成绩转换为等级制成绩。
 要求：如果输入的成绩在90分以上（含90分）输出A；80分-90分（不含90分）输出B；70分-80分（不含80分）输出C；60分-70分（不含70分）输出D；60分以下输出E。
-"""
+'''
 # score = float(input("请输入成绩: "))
 # if score >= 90:
 #     print("A")
@@ -75,13 +75,13 @@ f(x) =  x + 2   (-1 <= x <= 1)
 # else:
 #     print("E")
 
-"""
+'''
     判断输入的边长能否构成三角形，如果能则计算出三角形的周长和面积
     构成三角形的条件是:任意两边之和必须大于第三边。
     已知三边（海伦公式）:
     公式: S = √[s(s-a)(s-b)(s-c)]
     解释: 其中 s = (a+b+c)/2，表示半周长。这个公式在已知三边的情况下，可以直接计算面积。
-"""
+'''
 # a = float(input("a = "))
 # b = float(input("b = "))
 # c = float(input("c = "))
@@ -92,24 +92,24 @@ f(x) =  x + 2   (-1 <= x <= 1)
 #     print('面积: %.3f' % area)
 # else:
 #     print('不能构成三角形')
-"""
+'''
     用for循环实现1~100求和
-"""
+'''
 # sum = 0
 # for x in range(101):
 #     sum += x
 # print(sum)
-"""
+'''
     用for循环实现1~100之间的偶数求和
-"""
+'''
 # sum = 0
 # for x in range(2, 101, 2):
 #     sum += x
 # print(sum)
 
-"""
+'''
     猜数字游戏
-"""
+'''
 # import random
 #
 # answer = random.randint(1, 100)
@@ -130,18 +130,18 @@ f(x) =  x + 2   (-1 <= x <= 1)
 #             print('小了')
 #         counter += 1
 
-"""
+'''
     输出乘法口诀表(九九表)
-"""
+'''
 # for i in range(1, 10):
 #     for j in range(1, i + 1):
 #         print('%d*%d=%d' % (i, j, i * j), end='\t')
 #     print()
 
-"""
+'''
     输入一个正整数判断是不是素数。
 提示：素数指的是只能被1和自身整除的大于1的整数。
-"""
+'''
 # from math import sqrt
 #
 # num = int(input("请输入一个整数: "))
@@ -156,9 +156,9 @@ f(x) =  x + 2   (-1 <= x <= 1)
 # else:
 #     print("%d不是素数" % num)
 
-"""
+'''
     埃拉托斯特尼筛法，找出小于等于n的所有素数
-"""
+'''
 # 创建一个长度为n+1的列表，初始值都为True，表示都是素数
 # 找出小于等于100的所有素数
 # n = 100
@@ -175,10 +175,10 @@ f(x) =  x + 2   (-1 <= x <= 1)
 # # 返回所有素数的索引
 # print([i for i in range(2, n + 1) if primes[i]])
 
-"""
+'''
     输入两个正整数计算它们的最大公约数和最小公倍数
     提示：两个数的最大公约数是两个数的公共因子中最大的那个数；两个数的最小公倍数则是能够同时被两个数整除的最小的那个数
-"""
+'''
 # x = int(input('x = '))
 # y = int(input('y = '))
 # # 如果x大于y就交换x和y的值
@@ -191,42 +191,42 @@ f(x) =  x + 2   (-1 <= x <= 1)
 #         print('%d和%d的最大公约数是%d' % (x, y, factor))
 #         print('%d和%d的最小公倍数是%d' % (x, y, x * y // factor))
 #         break
-"""
+'''
     寻找水仙花数。
     说明：水仙花数也被称为超完全数字不变数、自恋数、自幂数、阿姆斯特朗数，它是一个3位数，该数字每个位上数字的立方之和正好等于它本身，例如：$1^3 + 5^3+ 3^3=153$。
-"""
+'''
 # for num in range(100,1000):
 #     low = num % 10
 #     mid = num // 10 % 10
 #     high = num // 100
 #     if num == low**3 + mid**3 + high**3:
 #         print(num)
-"""
+'''
     正整数的反转
     我们通过整除和求模运算分别找出了一个三位数的个位、十位和百位，这种小技巧在实际开发中还是常用的。用类似的方法，我们还可以实现将一个正整数反转，例如：将12345变成54321，代码如下所示。
-"""
+'''
 # num = int(input("请输入一个整数: "))
 # reversed_num = 0
 # while num > 0:
 #     reversed_num = reversed_num * 10 + num % 10
 #     num //= 10
 # print(reversed_num)
-"""
+'''
     百钱百鸡问题。
     公鸡5元一只，母鸡3元一只，小鸡1元三只，用100块钱买一百只鸡，问公鸡、母鸡、小鸡各有多少只？
-"""
+'''
 # for x in range(0, 20):
 #     for y in range(0, 33):
 #         z = 100 - x - y
 #         if 5 * x + 3 * y + z / 3 == 100:
 #             print('公鸡: %d只, 母鸡: %d只, 小鸡: %d只.' % (x, y, z))
-"""
+'''
 Craps赌博游戏
 我们设定玩家开始游戏时有1000元的赌注
 游戏结束的条件是玩家输光所有的赌注
 说明：CRAPS又称花旗骰，是美国拉斯维加斯非常受欢迎的一种的桌上赌博游戏。该游戏使用两粒骰子，玩家通过摇两粒骰子获得点数进行游戏。简单的规则是：玩家第一次摇骰子如果摇出了7点或11点，玩家胜；玩家第一次如果摇出2点、3点或12点，庄家胜；其他点数玩家继续摇骰子，
 如果玩家摇出了7点，庄家胜；如果玩家摇出了第一次摇的点数，玩家胜；其他点数，玩家继续要骰子，直到分出胜负。
-"""
+'''
 
 # from random import randint
 #
@@ -262,11 +262,11 @@ Craps赌博游戏
 #             needs_go_on = True
 # print('你破产了, 游戏结束!')
 
-"""
+'''
     生成斐波那契数列的前20个数。
-"""
+'''
 # def fibonacci_generator(n):
-#     """
+#     '''
 #     使用生成器表达式生成斐波那契数列
 #
 #     Args:
@@ -274,7 +274,7 @@ Craps赌博游戏
 #
 #     Yields:
 #       int: 斐波那契数
-#     """
+#     '''
 #
 #     a, b = 0, 1
 #     for _ in range(n):
@@ -288,7 +288,7 @@ Craps赌博游戏
 #
 #
 # def fibonacci_iterative(n):
-#     """
+#     '''
 #     使用循环迭代生成斐波那契数列
 #
 #     Args:
@@ -296,7 +296,7 @@ Craps赌博游戏
 #
 #     Returns:
 #       list: 斐波那契数列的前n个数
-#     """
+#     '''
 #
 #     fib = [0, 1]
 #     for i in range(2, n):
@@ -307,10 +307,10 @@ Craps赌博游戏
 # # 生成前20个斐波那契数列
 # result = fibonacci_iterative(20)
 # print(result)
-"""
+'''
     输出100以内所有的素数。
     素数指的是只能被1和自身整除的正整数（不包括1）。
-"""
+'''
 # from math import sqrt
 #
 # primeNumbers = []
@@ -336,9 +336,9 @@ Craps赌博游戏
 # print(city_country('上海','中国'))
 # print(city_country('北京','中国'))
 
-"""
+'''
     面向对象编程
-"""
+'''
 
 # class CuteCat:
 #     def __init__(self,cat_name,cat_color,cat_weight):
@@ -355,9 +355,9 @@ Craps赌博游戏
 # print(cat.color)
 # print(cat.weight)
 # cat.speak()
-"""
+'''
     对象继承
-"""
+'''
 # class Mammal:
 #     def __init__(self, name, sex):
 #         self.name = name
@@ -397,9 +397,9 @@ Craps赌博游戏
 # cat.eat()
 # cat.sleep()
 
-"""
+'''
     读写文件
-"""
+'''
 # from pathlib import Path
 # path = Path('/home/zbl/a.key')
 # # 读文件
@@ -410,9 +410,9 @@ Craps赌博游戏
 #     print(line)
 # # 写文件
 # path.write_text('hello world', encoding='utf-8')
-"""
+'''
     写文件作业
-"""
+'''
 # from pathlib import Path
 #
 # names = ''
@@ -423,9 +423,9 @@ Craps赌博游戏
 #
 # path = Path('/home/zbl/访客名单.txt')
 # path.write_text(names, encoding='utf-8')
-"""
+'''
     try except 异常捕获
-"""
+'''
 # try:
 #     print(1 / 1)
 # except ZeroDivisionError:
@@ -434,9 +434,9 @@ Craps赌博游戏
 #     print('没有发生异常')
 # finally:
 #     print('无论是否发生异常，都会执行')
-"""
+'''
     异常捕获作业两个数字相加
-"""
+'''
 # a = input('请输入第一个数字:')
 # b = input('请输入第二个数字:')
 # try:
@@ -447,18 +447,18 @@ Craps赌博游戏
 #     print(result)
 # finally:
 #     print('运行结束')
-"""
+'''
     断言 assert  测试用例 ,以test_开头即可 pytest测试库
-"""
+'''
 # def my_adder(x,y):
 #     return x+y
 #
 # def test_positive_with_positive():
 #     assert my_adder(1,2) == 3
 
-"""
+'''
     fixture 装饰器
-"""
+'''
 
 
 class Employee:
