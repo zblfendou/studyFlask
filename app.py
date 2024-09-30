f239facf402ff801b304125880c5163ab0fb9d3b
zhangbinlei <zhangbinlei@xinnet.com>
2024-09-30 10:38:46 +0800

    优化

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+
+from 根据域名获取cname或者A记录解析 import get_record
 
 app = Flask(__name__)
+
+
+@app.route('/getRecord', methods=['GET', 'POST'])
+def getRecord():
+    domain = request.args.get('domain')
+    type = request.args.get('type')
+    record = get_record(domain, type)
+    return record
+
 
 if __name__ == '__main__':
     app.run(debug=True)
